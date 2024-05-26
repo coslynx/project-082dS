676fb20fa8a040cb6dbdd75f34471758bd4ae3d2
Drishtant AKA DRIX10 <drixisgod@gmail.com>
2024-05-26 18:39:39 +0530

    Added: `src/utils/errorHandling.js`

---
+++ src/utils/errorHandling.js
@@ -0,0 +1,8 @@
+// File: src/utils/errorHandling.js
+
+const handleError = (error) => {
+  console.error('An error occurred:', error);
+  // Additional error handling logic can be added here
+};
+
+export { handleError };
